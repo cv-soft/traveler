d547373c70a7e0cbc446cbcdc21a09db89f7e635
symynjuk <mike.thats.my.name@gmail.com>
2019-03-19 19:42:07 +0200

    corrected init commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express'),
-    app = express();
-const indexRoutes = require('./routes/index.js');
+const express     = require('express'),
+      app         = express(),
+      indexRoutes = require('./routes/index.js');
+
 
 app.use(indexRoutes);
+
 app.listen(3001, function(){
     console.log("Traveler is running on port 3001");
 });
