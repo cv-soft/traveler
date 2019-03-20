37cb9ae410e86226b640ed29837ca9619b0144c7
symynjuk <mike.thats.my.name@gmail.com>
2019-03-20 14:47:27 +0200

    addedEjsModule

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
       router  = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("hello traveler!")
+    res.render("landing")
 });
 
 module.exports = router;
